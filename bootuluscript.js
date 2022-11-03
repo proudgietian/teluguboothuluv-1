4228f69f47dd231ab23a5576484f1aba7d54aea4
proudgietian <117358159+proudgietian@users.noreply.github.com>
2022-11-03 17:18:10 +0530

    Add files via upload

---
+++ bootuluscript.js
@@ -0,0 +1,36 @@
+alert("disclaimer: this website is made for entertainment purpose only!");
+
+document.querySelectorAll("button")[0].addEventListener("click",  handleclick1);
+
+ function handleclick1(){
+    var audio = new Audio('erripuka.mp3');
+    audio.play();
+ }
+
+ document.querySelectorAll("button")[1].addEventListener("click",  handleclick2);
+
+ function handleclick2(){
+    var audio = new Audio('moddagudu.mp3');
+    audio.play();
+ }
+
+ document.querySelectorAll("button")[2].addEventListener("click",  handleclick3);
+
+ function handleclick3(){
+    var audio = new Audio('mustilanga.mp3');
+    audio.play();
+ }
+
+ document.querySelectorAll("button")[3].addEventListener("click",  handleclick4);
+
+ function handleclick4(){
+    var audio = new Audio('neeabba.mp3');
+    audio.play();
+ }
+
+ document.querySelectorAll("button")[4].addEventListener("click",  handleclick5);
+
+ function handleclick5(){
+    var audio = new Audio('sigumodda.mp3');
+    audio.play();
+ }
---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll("button")[0].addEventListener("click", handleclick1);
+function handleclick1(){
+    var audio = new Audio('erripuka.mp3');
+    audio.play();
+ }
+
