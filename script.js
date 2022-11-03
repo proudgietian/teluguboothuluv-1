68ce2699819fe3ec6567a4c563add4c1bd3a243a
proudgietian <117358159+proudgietian@users.noreply.github.com>
2022-11-03 17:23:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.querySelectorAll("button")[0].addEventListener("click", handleclick1);
 function handleclick1(){
-    var audio = new Audio('erripuka.mp3');
+    var audio = new Audio('aaaah.mp3');
     audio.play();
  }
 
